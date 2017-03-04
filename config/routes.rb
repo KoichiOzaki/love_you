513b40b47defe0ed6105e11ce22adea52292fe23
KoichiOzaki <goingmyway19851224@gmail.com>
2017-03-04 12:31:08 +0900

    登録機能と表示周り

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,6 @@ Rails.application.routes.draw do
 
   get 'user_sessions/destroy'
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'users#show'
   resources :users
 end
